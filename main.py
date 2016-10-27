a27dd68e05c7487ef1da229306d1375d001249ae
xzxzxc <>
2016-10-27 16:02:40 +0000

    ^-^

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import StringIO
 import numpy
 from console import hud_alert
 from copy import deepcopy
-im=None
+v=None
+vMain=None
 
-def saveImage_action(sender):
-	photos.save_image(sender.superview['imageview1'].image)
-	hud_alert('Saved')
+
+def cancel_action(sender):
+	sender.superview.close()
 
 
 def update_im(v, im_n):
@@ -19,6 +20,34 @@ def update_im(v, im_n):
 	im_n.save(buff, format='bmp')
 	v['imageview1'].image=ui.Image.from_data(buff.getvalue())
 	buff.close()
+
+
+def fuckUp_load(sender):
+	global v, vMain
+	if im is None:
+		hud_alert('No image loaded')
+	v=ui.load_view('fuckupcolors')
+	v['imageview1'].image=vMain['imageview1'].image
+	v.present('full_screen', animated=False, hide_title_bar=True)
+
+
+def save_action(sender):
+	v_im=sender.superview['imageview1'].image
+	global im, vMain
+	buff=StringIO.StringIO(v_im.to_png())
+	im=Image.open(buff)
+	buff.close()
+	vMain['imageview1'].image=v_im
+	sender.superview.close()
+
+
+def saveLibrary_action(sender):
+	global im
+	if im is None:
+		return 
+	photos.save_image(sender.superview['imageview1'].image)
+	hud_alert('Saved')
+
 	
 
 @ui.in_background
@@ -26,42 +55,34 @@ def takePhoto_action(sender):
 	ph=photos.capture_image()
 	if ph is None:
 		return 
-	sender.superview['slider1'].value=0
+	if sender.superview.name=='fuckupcolors':
+		sender.superview['fuckUpSlider'].value=0
 	global im
 	im=ph
 	update_im(sender.superview, ph)
 	
 
 @ui.in_background
-def newImage_action(sender):
+def fromLibrary_action(sender):
 	ass = photos.pick_asset(assets=photos.get_assets(),title='Pick image to glitch')
 	if ass is None:
 		return 
-	sender.superview['slider1'].value=0
+	if sender.superview.name=='fuckupcolors':
+		sender.superview['fuckUpSlider'].value=0
 	sender.superview['imageview1'].image=ass.get_ui_image()
 	global im
 	im=ass.get_image()
 
 
-def slider_action(sender):
+def fuckUpSlider_action(sender):
 	v = sender.superview
-	n = int(200*numpy.log(1+v['slider1'].value))+1
+	n = int(200*numpy.log(1+v['fuckUpSlider'].value))+1
 	global im
 	if im is None:
 		return 
 	im_n = glitch.fuck_up_colors(im, n)
 	update_im(v, im_n)
 
-v = ui.load_view('main')
-newImage_action(v['imageview1'])
-
-if ui.get_screen_size()[1] >= 768:
-	# iPad
-	v.present('sheet')
-else:
-	# iPhone
-	v.present()
-
-
-
+vMain = ui.load_view('main')
+vMain.present('full_screen', animated=False, hide_title_bar=True)
 
