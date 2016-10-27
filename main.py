44e2833e8585998b87e96c7bab3bce0b4d882eb1
andrew <rybalko.andriy@gmail.com>
2016-10-27 19:23:16 +0300

    #_#

--- main.py
+++ main.py
@@ -7,82 +7,79 @@ import StringIO
 import numpy
 from console import hud_alert
 from copy import deepcopy
-v=None
-vMain=None
+
+v = None
+vMain = None
 
 
 def cancel_action(sender):
-	sender.superview.close()
+    sender.superview.close()
 
 
-def update_im(v, im_n):
-	buff=StringIO.StringIO()
-	im_n.save(buff, format='bmp')
-	v['imageview1'].image=ui.Image.from_data(buff.getvalue())
-	buff.close()
+def from_norm_to_ui(norm_im):
+    buff = StringIO.StringIO()
+    norm_im.save(buff, format='bmp')
+    ui_im = ui.Image.from_data(buff.getvalue())
+    buff.close()
+    return ui_im
+
+
+def from_ui_to_norm(ui_im):
+    return Image.open(ui_im.to_png())
 
 
 def fuckUp_load(sender):
-	global v, vMain
-	if im is None:
-		hud_alert('No image loaded')
-	v=ui.load_view('fuckupcolors')
-	v['imageview1'].image=vMain['imageview1'].image
-	v.present('full_screen', animated=False, hide_title_bar=True)
+    global v, vMain
+    if sender.superview['imageview1'].image is None:
+        hud_alert('No image loaded')
+    v = ui.load_view('fuckupcolors')
+    v['imageview1'].image = vMain['imageview1'].image
+    v.present('full_screen', animated=False, hide_title_bar=True)
 
 
 def save_action(sender):
-	v_im=sender.superview['imageview1'].image
-	global im, vMain
-	buff=StringIO.StringIO(v_im.to_png())
-	im=Image.open(buff)
-	buff.close()
-	vMain['imageview1'].image=v_im
-	sender.superview.close()
+    v_im = sender.superview['imageview1'].image
+    global vMain
+    vMain['imageview1'].image = v_im
+    sender.superview.close()
 
 
 def saveLibrary_action(sender):
-	global im
-	if im is None:
-		return 
-	photos.save_image(sender.superview['imageview1'].image)
-	hud_alert('Saved')
+    if sender.superview['imageview1'].image is None:
+        return
+    photos.save_image(sender.superview['imageview1'].image)
+    hud_alert('Saved')
 
-	
 
 @ui.in_background
 def takePhoto_action(sender):
-	ph=photos.capture_image()
-	if ph is None:
-		return 
-	if sender.superview.name=='fuckupcolors':
-		sender.superview['fuckUpSlider'].value=0
-	global im
-	im=ph
-	update_im(sender.superview, ph)
-	
+    ph = photos.capture_image()
+    if ph is None:
+        return
+    if sender.superview.name == 'fuckupcolors':
+        sender.superview['fuckUpSlider'].value = 0
+    sender.superview['imageview1'].image = from_norm_to_ui(ph)
+
 
 @ui.in_background
 def fromLibrary_action(sender):
-	ass = photos.pick_asset(assets=photos.get_assets(),title='Pick image to glitch')
-	if ass is None:
-		return 
-	if sender.superview.name=='fuckupcolors':
-		sender.superview['fuckUpSlider'].value=0
-	sender.superview['imageview1'].image=ass.get_ui_image()
-	global im
-	im=ass.get_image()
+    ass = photos.pick_asset(assets=photos.get_assets(), title='Pick image to glitch')
+    if ass is None:
+        return
+    if sender.superview.name == 'fuckupcolors':
+        sender.superview['fuckUpSlider'].value = 0
+    sender.superview['imageview1'].image = ass.get_ui_image()
 
 
 def fuckUpSlider_action(sender):
-	v = sender.superview
-	n = int(200*numpy.log(1+v['fuckUpSlider'].value))+1
-	global im
-	if im is None:
-		return 
-	im_n = glitch.fuck_up_colors(im, n)
-	update_im(v, im_n)
+    v = sender.superview
+    n = int(200 * numpy.log(1 + v['fuckUpSlider'].value)) + 1
+    im = v['imageview1'].image
+    if im is None:
+        return
+    im_n = glitch.fuck_up_colors(from_ui_to_norm(im), n)
+    v['imageview1'].image=from_norm_to_ui(im_n)
+
 
 vMain = ui.load_view('main')
 vMain.present('full_screen', animated=False, hide_title_bar=True)
-
