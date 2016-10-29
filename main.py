803d0b21b46cf6a9d44987b934ddab179452423f
andrew <rybalko.andriy@gmail.com>
2016-10-29 21:47:13 +0300

    uihhui

--- main.py
+++ main.py
@@ -143,7 +143,6 @@ def shiftSlider_action(sender):
     y0 = int((region.y + 15)*im.size[1]/v['view1'].height)
     gl_heigth = int((region.height - 30) *im.size[1]/v['view1'].height)
     dy = int(0.005 * im.size[1])
-    print '%f,%f,%f,%f,%f'%(dy,x0,y0,gl_heigth,gl_widt)
     im_n=im.copy()
     if v['dirControl'].selected_index==0:
         for i in range(int(intens*100)):
@@ -160,14 +159,12 @@ def shiftSlider_action(sender):
         for i in range(int(intens*100)):
             y = [rn.randint(y0, y0 + gl_heigth - dy)]
             if y[0] - y0 < gl_heigth / 2:
-                dx = int(numpy.sqrt(y[0] - y0)/gl_heigth)*xShift
+                dx = int(numpy.sqrt((y[0] - y0)/gl_heigth*xShift))
             else:
-                dx = int(numpy.sqrt(y0 + gl_heigth - y0)/gl_heigth)*xShift
+                dx = int(numpy.sqrt((y0 + gl_heigth - y[0])/gl_heigth*xShift))
             x = [rn.randint(x0, x0 + gl_widt) + dx, rn.randint(x0, x0 + gl_widt) + dx]
             y.append(y[0] + dy)
             x.sort()
-            print im.size
-            print x, y
             glitch.glitch_shift_right(im_n, rn.randint(int(0.75*xShift), xShift), x[0], y[0], x[1], y[1])
     v['view1']['imageview1'].image, buff = from_norm_to_ui(im_n)
     buff.close()
